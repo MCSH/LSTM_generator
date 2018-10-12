b92a5650c5b02cf2d05dd26260977623ede59350
Sajjad Heydari <mcshemail@gmail.com>
2018-10-12 23:04:00 +0330

    Fixed normalizer error

--- normalizer.py
+++ normalizer.py
@@ -1,6 +1,4 @@
 
 
 def normalizer(text):
-    return text.replace('ي', 'ی')
-                .replace('ة', 'ه')
-                .replace(ٌ'‌','')
+    return text.replace('ي', 'ی').replace('ة', 'ه')
